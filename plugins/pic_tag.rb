affc9b174d3035de45421c441bd454e3938941b4
David Stosik <david.stosik+git-noreply@gmail.com>
2024-02-09 12:02:29 +0900

    Implement `{% pic ... %}` and `{% pic_cap ... %}...` tags

---
+++ plugins/pic_tag.rb
@@ -0,0 +1,34 @@
+class PicTag < SiteBuilder
+  def build
+    liquid_tag :pic
+    liquid_tag :pic_cap, as_block: true
+  end
+
+  private
+
+  def pic_cap(params, tag)
+    <<~HTML
+      <figure>
+        #{pic(params, tag)}
+        <figcaption>#{tag.content}</figcaption>
+      </figure>
+    HTML
+  end
+
+  def pic(params, tag)
+    name, alt = params.split(",", 2).map(&:strip)
+    alt ||= name
+
+    date = tag.context["page"].date.to_date
+
+    file = tag.context["site"].static_files.find do |file|
+      file.relative_path =~ %r{images/#{date}/#{name}}
+    end
+
+    <<~HTML
+      <a href="#{file.url}" target="_blank">
+        <img width="400px" alt="#{alt}" class="image" src="#{file.url}" />
+      </a>
+    HTML
+  end
+end
