d8a75f433fa3292025d5d3f1bba0ff0ffa2f4cf4
David Stosik <816901+davidstosik@users.noreply.github.com>
2026-05-11 23:14:37 +0900

    Force UTF-8 encoding on text files without explicit charset served in dev mode

--- server/roda_app.rb
+++ server/roda_app.rb
@@ -14,6 +14,27 @@ class RodaApp < Bridgetown::Rack::Roda
   # uncomment the gem dependency in your `Gemfile` as well):
   # plugin :bridgetown_routes
 
+  after do |res|
+    next unless res
+
+    headers = res[1]
+    content_type = headers["Content-Type"]
+
+    next unless content_type
+    next if content_type.include?("charset=")
+
+    textual =
+      content_type.start_with?("text/") ||
+      content_type.start_with?("application/json") ||
+      content_type.start_with?("application/javascript") ||
+      content_type.start_with?("application/xml") ||
+      content_type.start_with?("application/rss+xml") ||
+      content_type.start_with?("application/atom+xml") ||
+      content_type.start_with?("image/svg+xml")
+
+    headers["Content-Type"] = "#{content_type}; charset=utf-8" if textual
+  end
+
   route do |r|
     # Load Roda routes in server/routes (and src/_routes via `bridgetown-routes`)
     r.bridgetown
